2ccc8aa0a7676f465c9eff68163bab73197a6d97
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-03-01 09:21:34 -0500

    building api connections

--- src/users.js
+++ src/users.js
@@ -1,23 +1,49 @@
-const { connectDb } = require('./dbConnect')
+const { connectDb } = require("./dbConnect");
 
-exports.createUser = ( req, res) => {
-    //first, Need Validation. (Email, Password)
-    if(!req.body || !req.body.email || !req.body.password){
-        //invalid request
-        res.status(400).send('Invalid request')
-        return
-    }
-    const newUser = {     //Forcing shape/content of a new user.
-        email: req.body.email,
-        password: req.body.password,
+exports.createUser = (req, res) => {
+  //first, Need Validation. (Email, Password)
+  if (!req.body || !req.body.email || !req.body.password) {
+    //invalid request
+    res.status(400).send("Invalid request");
+    return;
+  }
+  const newUser = {
+    //Forcing shape/content of a new user.
+    email: req.body.email.toLowerCase(),
+    password: req.body.password,
+    isAdmin: false,
+    useRole: 5,
+  };
+  const db = connectDb();
+  db.collection("users")
+    .add(newUser)
+    .then((doc) => {
+      const user = {
+        //this will become the payload.
+        id: doc.id,
+        email: newUser.email,
         isAdmin: false,
         useRole: 5,
-    }
-    const db = connectDb()
-    db.collection('users').add(newUser)
-    .then(doc => {
-        
-        res.send(201).send('Account created')
+      };
+      res.send(201).send({
+        success: true,
+        message: "Account created",
+        token: user, // add this to token later
+      });
     })
-    .catch(err => res.status(500).send(err))
-}
+    .catch((err) => res.status(500).send(err));
+};
+
+exports.loginUser = (req, res) => {
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send("Invalid request");
+    return;
+  }
+  const db = connectDb();
+  db.collection("users")
+  .where("email", "===", req.body.email.toLowerCase())
+  .where('password', '===', req.body.password)
+  .get()
+   .then()
+   .catch((err) => res.status(500).send(err));
+};
